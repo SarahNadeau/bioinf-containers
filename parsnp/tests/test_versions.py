6f40b63ebcd52e117c2d5e9d19ab95a447d0785c
SarahNadeau <san62@cornell.edu>
2021-12-13 11:30:46 -0500

    deleted comment lines since decided to have each class of tests in a different file

--- parsnp/tests/test_versions.py
+++ parsnp/tests/test_versions.py
@@ -10,12 +10,6 @@ class TestVersions(unittest.TestCase):
         result = subprocess.run(bash_cmd, shell=True, stdout=PIPE)
         self.assertEqual(result.stdout, b"parsnp 1.5.6\n")
 
-# TODO: put in data, check what comes out looks right
-# class TestOutputFormat(unittest.TestCase):
-#
-#     def test_parsnp_output_format(self):
-#         bash_cmd = "parsnp"
-
 
 if __name__ == '__main__':
     unittest.main()
