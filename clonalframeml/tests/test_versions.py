acb25f56f3bb6175b29c6f00af63ff7e51e3d56a
SarahNadeau <san62@cornell.edu>
2021-12-15 10:55:24 -0500

    add ClonalFrameML dockerfile & basic version test

--- clonalframeml/tests/test_versions.py
+++ clonalframeml/tests/test_versions.py
@@ -1,7 +1,7 @@
 import unittest
 import subprocess
 from subprocess import PIPE
-
+import re
 
 class TestVersions(unittest.TestCase):
 
@@ -9,6 +9,11 @@ class TestVersions(unittest.TestCase):
         bash_cmd = "ClonalFrameML -version"
         result = subprocess.run(bash_cmd, shell=True, stdout=PIPE)
         self.assertEqual(result.stdout, b"ClonalFrameML v1.12\n")
+
+    def test_r_version(self):
+        bash_cmd = "R --version"
+        result = subprocess.run(bash_cmd, shell=True, stdout=PIPE)
+        assert re.search(b"4.0.5 \(2021-03-31\)", result.stdout)
 
 
 if __name__ == '__main__':
