0d4449f68f5031821292ecf812b6c97d8c8ed61c
SarahNadeau <san62@cornell.edu>
2021-12-15 08:44:54 -0500

    pin GH actions runner to ubuntu-20.04, output RAxML tree for sanity check that it's not equivalent to FastTree tree even if RF = 0

--- parsnp/tests/test_real_data.py
+++ parsnp/tests/test_real_data.py
@@ -20,6 +20,9 @@ class TestOutput(unittest.TestCase):
             print(f.read())
         with open("/data/outdir_parsnp_fasttree/parsnp.tree", 'r') as f:
             print(f.read())
+        print("RAxML results for debugging (tree only):")
+        with open("/data/outdir_raxml_fasttree/parsnp.tree", 'r') as f:
+            print(f.read())
 
     def test_alignments_equal(self):
         with open("/data/outdir_parsnp_fasttree/parsnp.xmfa.checksum", 'r') as f:
@@ -49,21 +52,21 @@ class TestOutput(unittest.TestCase):
     # Run only if in GitHub actions workflow because builds on different machines yield different alignments, treefiles
     @unittest.skipIf(not is_github, "Skipping because checksums are only valid for image built by GitHub actions workflow.")
     def test_alignments_vs_ref_checksum(self):
-        aln_ref_hash = "dad980b7c9a043b335817dbbf4805967a04142b5878bc834faf0ae91fdf6e635"
+        aln_ref_hash = "4f7f0c894af7598740e762ceae2d0694c3fa7dc46dcebc2ebb52cfe75686ee09"
         with open("/data/outdir_parsnp_raxml/parsnp.xmfa.checksum", 'r') as f:
             raxml_aln_test_hash = f.readlines()[0].split(" ")[0]
         self.assertEqual(raxml_aln_test_hash, aln_ref_hash)
 
     @unittest.skipIf(not is_github, "Skipping because checksums are only valid for image built by GitHub actions workflow.")
     def test_fasttree_treefile_vs_ref_checksum(self):
-        fasttree_tree_ref_hash = "15232b68d14929f3efa9d6c315a4f8e8e41ef462b6951df157d1f5095a45263b"
+        fasttree_tree_ref_hash = "edc556aa1a74de7d28332456e65bee4163d41fa36a2d380e0eb0170055fcbfb5"
         with open("/data/outdir_parsnp_fasttree/parsnp.tree.checksum", 'r') as f:
             fasttree_tree_test_hash = f.readlines()[0].split(" ")[0]
         self.assertEqual(fasttree_tree_test_hash, fasttree_tree_ref_hash)
 
     @unittest.skipIf(not is_github, "Skipping because checksums are only valid for image built by GitHub actions workflow.")
     def test_raxml_treefile_vs_ref_checksum(self):
-        raxml_tree_ref_hash = "f4fe7f25d432694e27329f6c16472a8b02a19e97274e536d95c2deb52bb44445"
+        raxml_tree_ref_hash = "14daa21714d8c5b61540bd0b0275cb9472f454237756fda992428002a241475f"
         with open("/data/outdir_parsnp_raxml/parsnp.tree.checksum", 'r') as f:
             raxml_tree_test_hash = f.readlines()[0].split(" ")[0]
         self.assertEqual(raxml_tree_test_hash, raxml_tree_ref_hash)
