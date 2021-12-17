1efa2ccd618a13cd8d990da8c7edded81d06530e
SarahNadeau <san62@cornell.edu>
2021-12-17 11:43:43 -0500

    clean up ParSNP, ClonalFrameML containers, install R from CRAN not source

--- clonalframeml/1.12/tests/test_versions.py
+++ clonalframeml/1.12/tests/test_versions.py
@@ -13,7 +13,7 @@ class TestVersions(unittest.TestCase):
     def test_r_version(self):
         bash_cmd = "R --version"
         result = subprocess.run(bash_cmd, shell=True, stdout=PIPE)
-        assert re.search(b"4.0.5 \(2021-03-31\)", result.stdout)
+        assert re.search(b"4\.[0-9]\.[0-9]", result.stdout)
 
 
 if __name__ == '__main__':
